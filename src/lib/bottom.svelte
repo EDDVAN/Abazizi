718ba89086bf68bc890f4f42f93bdc71f3f8ce06
EDDVAN <akakumaboi@gmail.com>
2023-05-21 17:23:42 +0100

    skeleton elements

---
+++ src/lib/bottom.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+</script>
+
+<section>
+  <nav>
+    <div id="about"><span>A</span>bout</div>
+    <div id="education"><span>E</span>ducation & <span>E</span>xperience</div>
+    <div id="tech"><span>T</span>ech <span>S</span>tack</div>
+  </nav>
+  <article />
+</section>
+
+<style lang="scss">
+  section {
+    width: 100%;
+    height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    scroll-snap-align: start;
+  }
+  nav {
+    width: 100%;
+    height: 10hv;
+    padding: 0 2rem;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    font-size: 250%;
+    color: black;
+    gap: 0.75rem;
+    z-index: 2;
+    & > #about {
+      border-top: 1rem solid #e91312;
+      & > span {
+        color: #e91312;
+        font-weight: 600;
+      }
+    }
+    & > #education {
+      border-top: 1rem solid #ff890d;
+      & > span {
+        color: #ff890d;
+        font-weight: 600;
+      }
+    }
+    & > #tech {
+      border-top: 1rem solid #06e8e4;
+      & > span {
+        color: #06e8e4;
+        font-weight: 600;
+      }
+    }
+    & > div {
+      background-color: white;
+      padding: 1rem 2rem;
+      box-sizing: border-box;
+    }
+  }
+  article {
+    z-index: 1;
+    height: 90vh;
+    width: 100%;
+    padding: 2rem;
+    box-sizing: border-box;
+    background-color: white;
+    outline: 1rem solid #e91312;
+  }
+</style>
---
+++ src/lib/top.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+</script>
+
+<section>
+  <div id="left">
+    <h3 id="top-h3">My name is</h3>
+    <h1>BAZIZI ALI</h1>
+    <h3 id="bot-h3">And I'm a <span>Web Developer</span></h3>
+  </div>
+  <div id="right">
+    <img src="/i.png" alt="Bazizi Ali" />
+  </div>
+</section>
+
+<style lang="scss">
+  section {
+    width: 100%;
+    height: 100vh;
+    padding: 2rem;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    place-items: center;
+    scroll-snap-align: start;
+    justify-content: space-around;
+    & > #left {
+      display: flex;
+      flex-direction: column;
+      position: relative;
+      padding: 0 10rem;
+      cursor: pointer;
+      & > * {
+        padding: 0;
+        margin: 0;
+      }
+      & > #top-h3 {
+        text-align: left;
+      }
+      & > #bot-h3 {
+        text-align: right;
+      }
+      & > h3 {
+        font-size: 150%;
+        font-family: "Phudu", cursive;
+        font-weight: 300;
+        & > span {
+          font-weight: 600;
+        }
+      }
+      & > h1 {
+        font-size: 800%;
+        font-family: "Qualy", cursive;
+      }
+    }
+    & > #right {
+      place-items: center;
+      & > img {
+        height: 50rem;
+      }
+    }
+  }
+</style>
