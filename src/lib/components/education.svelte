bd81eeace0756ae2472f3ddbd664783ae7fee328
EDDVAN <akakumaboi@gmail.com>
2024-10-21 22:01:17 +0100

    actually fix the mobile nav

--- src/lib/components/education.svelte
+++ src/lib/components/education.svelte
@@ -1,175 +1,194 @@
-<section
-	class="md:flex hidden flex-col gap-4 justify-between items-left h-[calc(35vh)] border-y-[1px] border-slate py-2 md:px-24 px-8"
-	id="education"
->
-	<div class="flex flex-grow w-full py-6">
-		<div class="w-1/4 flex justify-center items-center text-center">
-			<div
-				class="flex flex-col justify-center items-center p-4 w-2/3 h-1/2 border-2 border-emerald relative overflow-hidden cursor-pointer education-card"
-			>
-				<h1 class="text-xl font-header text-white">High School</h1>
-				<h2 class="text-base text-slate">Physics & Chemistry</h2>
-				<h3 class="text-md text-slate">2019</h3>
-				<div
-					class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-emerald top-0 left-full text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-				>
-					Finished High School, and started my studies in Computer Science.
-				</div>
-			</div>
-			<div class="h-1 w-1/3 bg-gradient-to-r from-emerald via-lime to-amber-500"></div>
-		</div>
-		<div class="w-3/4 flex justify-center items-center">
-			<div class="flex flex-col justify-center items-center w-2/12 h-full">
-				<div class="h-1/2 w-full bg-gradient-to-r from-amber-500 to-orangevariant py-1 pl-1">
-					<div class="h-full w-full bg-dark"></div>
-				</div>
-			</div>
-			<div class="flex flex-col justify-center items-center w-10/12 h-full gap-4 py-4">
-				<div class="w-full h-1/2 flex justify-center items-center">
-					<div
-						class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
-					>
-						<h1 class="text-xl font-header text-white">Specialized Technicien</h1>
-						<h2 class="text-base text-slate">Computer Science</h2>
-						<h3 class="text-md text-slate">2019 - 2021</h3>
-						<div
-							class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-orangevariant top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-						>
-							The first step of my CS journey, where I grasped the fundamentals of Algorithms,
-							Desktop, Web and Mobile Development.
-						</div>
-					</div>
-					<div class="h-1 w-1/3 bg-gradient-to-r from-orangevariant to-pink"></div>
-					<div
-						class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-pink relative overflow-hidden cursor-pointer education-card"
-					>
-						<h1 class="text-xl font-header text-white">Bachelor's Degree</h1>
-						<h2 class="text-base text-slate">Computer Science</h2>
-						<h3 class="text-md text-slate">2024 - 2025</h3>
-						<div
-							class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-pink top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-						>
-							Currently continuing my education in a one year program in software engineering at
-							ENSA Fes
-						</div>
-					</div>
-				</div>
-				<div class="w-full h-1/2 flex justify-center items-center">
-					<div
-						class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
-					>
-						<h1 class="text-xl font-header text-white">1337 - 42 Network</h1>
-						<h2 class="text-base text-slate">Computer Architecture</h2>
-						<h3 class="text-md text-slate">2021 - 2023</h3>
-						<div
-							class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-orangevariant -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-						>
-							A switch from traditional studies to peer learning, with projects ranging from a LibC
-							equivalent to a fully working Shell.
-						</div>
-					</div>
-					<div class="h-1 w-1/3 bg-gradient-to-r from-orangevariant to-pink"></div>
-					<div
-						class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-pink relative overflow-hidden cursor-pointer education-card"
-					>
-						<h1 class="text-xl font-header text-white">Self Learning</h1>
-						<h2 class="text-base text-slate">Web Development</h2>
-						<h3 class="text-md text-slate">2019 - Retirement</h3>
-						<div
-							class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-pink -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-						>
-							A self-learning journey, with a focus on Web Development, every day I'm challanging
-							myself to learn something new.
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+<script>
+	import { onDestroy } from 'svelte';
 
-<section
-	class="flex md:hidden flex-col justify-between gap-8 items-center h-fit border-y-[1px] border-slate py-8 px-8 {$$restProps.class}"
-	id="education"
->
-	<div class="flex flex-col justify-center items-center p-6 w-full bg-emerald">
-		<h1 class="text-3xl font-header font-semibold text-dark">Education</h1>
-		<h3 class="text-lg text-darkvariant">Click on the cards to learn more</h3>
-	</div>
-	<div
-		class="flex flex-col justify-center items-center p-6 w-full border-2 border-emerald relative overflow-hidden cursor-pointer education-card"
+	let width = window.innerWidth;
+
+	function handleResize() {
+		width = window.innerWidth;
+	}
+
+	window.addEventListener('resize', handleResize);
+
+	// Remove event listener when the component is destroyed
+	onDestroy(() => {
+		window.removeEventListener('resize', handleResize);
+	});
+</script>
+
+{#if width > 768}
+	<section
+		class="md:flex hidden flex-col gap-4 justify-between items-left h-[calc(35vh)] border-y-[1px] border-slate py-2 md:px-24 px-8"
+		id="education"
 	>
-		<h1 class="text-xl font-header text-white">High School</h1>
-		<h2 class="text-base text-slate">Physics & Chemistry</h2>
-		<h3 class="text-md text-slate">2019</h3>
+		<div class="flex flex-grow w-full py-6">
+			<div class="w-1/4 flex justify-center items-center text-center">
+				<div
+					class="flex flex-col justify-center items-center p-4 w-2/3 h-1/2 border-2 border-emerald relative overflow-hidden cursor-pointer education-card"
+				>
+					<h1 class="text-xl font-header text-white">High School</h1>
+					<h2 class="text-base text-slate">Physics & Chemistry</h2>
+					<h3 class="text-md text-slate">2019</h3>
+					<div
+						class="absolute h-full w-full flex flex-col justify-center items-center
+						 bg-emerald top-0 left-full text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+					>
+						Finished High School, and started my studies in Computer Science.
+					</div>
+				</div>
+				<div class="h-1 w-1/3 bg-gradient-to-r from-emerald via-lime to-amber-500"></div>
+			</div>
+			<div class="w-3/4 flex justify-center items-center">
+				<div class="flex flex-col justify-center items-center w-2/12 h-full">
+					<div class="h-1/2 w-full bg-gradient-to-r from-amber-500 to-orangevariant py-1 pl-1">
+						<div class="h-full w-full bg-dark"></div>
+					</div>
+				</div>
+				<div class="flex flex-col justify-center items-center w-10/12 h-full gap-4 py-4">
+					<div class="w-full h-1/2 flex justify-center items-center">
+						<div
+							class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
+						>
+							<h1 class="text-xl font-header text-white">Specialized Technicien</h1>
+							<h2 class="text-base text-slate">Computer Science</h2>
+							<h3 class="text-md text-slate">2019 - 2021</h3>
+							<div
+								class="absolute h-full w-full flex flex-col justify-center items-center
+						 bg-orangevariant top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+							>
+								The first step of my CS journey, where I grasped the fundamentals of Algorithms,
+								Desktop, Web and Mobile Development.
+							</div>
+						</div>
+						<div class="h-1 w-1/3 bg-gradient-to-r from-orangevariant to-pink"></div>
+						<div
+							class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-pink relative overflow-hidden cursor-pointer education-card"
+						>
+							<h1 class="text-xl font-header text-white">Bachelor's Degree</h1>
+							<h2 class="text-base text-slate">Computer Science</h2>
+							<h3 class="text-md text-slate">2024 - 2025</h3>
+							<div
+								class="absolute h-full w-full flex flex-col justify-center items-center
+						 bg-pink top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+							>
+								Currently continuing my education in a one year program in software engineering at
+								ENSA Fes
+							</div>
+						</div>
+					</div>
+					<div class="w-full h-1/2 flex justify-center items-center">
+						<div
+							class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
+						>
+							<h1 class="text-xl font-header text-white">1337 - 42 Network</h1>
+							<h2 class="text-base text-slate">Computer Architecture</h2>
+							<h3 class="text-md text-slate">2021 - 2023</h3>
+							<div
+								class="absolute h-full w-full flex flex-col justify-center items-center
+						 bg-orangevariant -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+							>
+								A switch from traditional studies to peer learning, with projects ranging from a
+								LibC equivalent to a fully working Shell.
+							</div>
+						</div>
+						<div class="h-1 w-1/3 bg-gradient-to-r from-orangevariant to-pink"></div>
+						<div
+							class="flex flex-col justify-center items-center w-1/3 h-full border-2 border-pink relative overflow-hidden cursor-pointer education-card"
+						>
+							<h1 class="text-xl font-header text-white">Self Learning</h1>
+							<h2 class="text-base text-slate">Web Development</h2>
+							<h3 class="text-md text-slate">2019 - Retirement</h3>
+							<div
+								class="absolute h-full w-full flex flex-col justify-center items-center
+						 bg-pink -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+							>
+								A self-learning journey, with a focus on Web Development, every day I'm challanging
+								myself to learn something new.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+{:else}
+	<section
+		class="flex md:hidden flex-col justify-between gap-8 items-center h-fit border-y-[1px] border-slate py-8 px-8 {$$restProps.class}"
+		id="education"
+	>
+		<div class="flex flex-col justify-center items-center p-6 w-full bg-emerald">
+			<h1 class="text-3xl font-header font-semibold text-dark">Education</h1>
+			<h3 class="text-lg text-darkvariant">Click on the cards to learn more</h3>
+		</div>
 		<div
-			class="absolute h-full w-full flex flex-col justify-center items-center
+			class="flex flex-col justify-center items-center p-6 w-full border-2 border-emerald relative overflow-hidden cursor-pointer education-card"
+		>
+			<h1 class="text-xl font-header text-white">High School</h1>
+			<h2 class="text-base text-slate">Physics & Chemistry</h2>
+			<h3 class="text-md text-slate">2019</h3>
+			<div
+				class="absolute h-full w-full flex flex-col justify-center items-center
 				 bg-emerald -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-		>
-			Finished High School, and started my studies in Computer Science.
+			>
+				Finished High School, and started my studies in Computer Science.
+			</div>
 		</div>
-	</div>
-	<div
-		class="flex flex-col justify-center items-center w-full p-6 border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
-	>
-		<h1 class="text-xl font-header text-white">Specialized Technicien</h1>
-		<h2 class="text-base text-slate">Computer Science</h2>
-		<h3 class="text-md text-slate">2019 - 2021</h3>
 		<div
-			class="absolute h-full w-full flex flex-col justify-center items-center
+			class="flex flex-col justify-center items-center w-full p-6 border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
+		>
+			<h1 class="text-xl font-header text-white">Specialized Technicien</h1>
+			<h2 class="text-base text-slate">Computer Science</h2>
+			<h3 class="text-md text-slate">2019 - 2021</h3>
+			<div
+				class="absolute h-full w-full flex flex-col justify-center items-center
                              bg-orangevariant top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-		>
-			The first step of my CS journey, where I grasped the fundamentals of Algorithms, Desktop, Web
-			and Mobile Development.
+			>
+				The first step of my CS journey, where I grasped the fundamentals of Algorithms, Desktop,
+				Web and Mobile Development.
+			</div>
 		</div>
-	</div>
-	<div
-		class="flex flex-col justify-center items-center w-full p-6 border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
-	>
-		<h1 class="text-xl font-header text-white">1337 - 42 Network</h1>
-		<h2 class="text-base text-slate">Computer Architecture</h2>
-		<h3 class="text-md text-slate">2021 - 2023</h3>
 		<div
-			class="absolute h-full w-full flex flex-col justify-center items-center
+			class="flex flex-col justify-center items-center w-full p-6 border-2 border-orangevariant relative overflow-hidden cursor-pointer education-card"
+		>
+			<h1 class="text-xl font-header text-white">1337 - 42 Network</h1>
+			<h2 class="text-base text-slate">Computer Architecture</h2>
+			<h3 class="text-md text-slate">2021 - 2023</h3>
+			<div
+				class="absolute h-full w-full flex flex-col justify-center items-center
                              bg-orangevariant -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-		>
-			A switch from traditional studies to peer learning, with projects ranging from a LibC
-			equivalent to a fully working Shell.
+			>
+				A switch from traditional studies to peer learning, with projects ranging from a LibC
+				equivalent to a fully working Shell.
+			</div>
 		</div>
-	</div>
-	<div
-		class="flex flex-col justify-center items-center w-full p-6 border-2 border-pink relative overflow-hidden cursor-pointer education-card"
-	>
-		<h1 class="text-xl font-header text-white">Bachelor's Degree</h1>
-		<h2 class="text-base text-slate">Computer Science</h2>
-		<h3 class="text-md text-slate">2024 - 2025</h3>
 		<div
-			class="absolute h-full w-full flex flex-col justify-center items-center
+			class="flex flex-col justify-center items-center w-full p-6 border-2 border-pink relative overflow-hidden cursor-pointer education-card"
+		>
+			<h1 class="text-xl font-header text-white">Bachelor's Degree</h1>
+			<h2 class="text-base text-slate">Computer Science</h2>
+			<h3 class="text-md text-slate">2024 - 2025</h3>
+			<div
+				class="absolute h-full w-full flex flex-col justify-center items-center
                              bg-pink top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
-		>
-			Currently continuing my education in a one year program in software engineering at ENSA Fes
+			>
+				Currently continuing my education in a one year program in software engineering at ENSA Fes
+			</div>
 		</div>
-	</div>
-	<div
-		class="flex flex-col justify-center items-center w-full p-6 border-2 border-pink relative overflow-hidden cursor-pointer education-card"
-	>
-		<h1 class="text-xl font-header text-white">Self Learning</h1>
-		<h2 class="text-base text-slate">Web Development</h2>
-		<h3 class="text-md text-slate">2019 - Retirement</h3>
 		<div
-			class="absolute h-full w-full flex flex-col justify-center items-center
-                             bg-pink -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+			class="flex flex-col justify-center items-center w-full p-6 border-2 border-pink relative overflow-hidden cursor-pointer education-card"
 		>
-			A self-learning journey, with a focus on Web Development, every day I'm challanging myself to
-			learn something new.
+			<h1 class="text-xl font-header text-white">Self Learning</h1>
+			<h2 class="text-base text-slate">Web Development</h2>
+			<h3 class="text-md text-slate">2019 - Retirement</h3>
+			<div
+				class="absolute h-full w-full flex flex-col justify-center items-center
+                             bg-pink -top-full left-0 text-left text-dark font-semibold p-2 duration-300 ease-in-out"
+			>
+				A self-learning journey, with a focus on Web Development, every day I'm challanging myself
+				to learn something new.
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+{/if}
 
 <style lang="scss">
 	.education-card:hover {
